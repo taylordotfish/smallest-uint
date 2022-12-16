bd4b990a3d670afede09423886ab5b318441a210
taylor.fish <contact@taylor.fish>
2022-12-16 06:18:16 -0800

    Update license notices

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,11 @@
 /*
  * Copyright 2021 taylor.fish <contact@taylor.fish>
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This file is part of smallest-uint.
+ *
+ * smallest-uint is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use smallest-uint except in compliance
+ * with the License. You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
